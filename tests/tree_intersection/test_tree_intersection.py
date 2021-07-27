5c6815417ac325e7903a0f9c7f718a0f4e194112
suhaib079 <suhaibabdennabi33@gmail.com>
2021-07-27 19:13:04 +0300

    l

--- tests/tree_intersection/test_tree_intersection.py
+++ tests/tree_intersection/test_tree_intersection.py
@@ -4,21 +4,21 @@ import pytest
 @pytest.fixture
 def fir_tree():
     Bt1 = BinaryTree()
-    Bt1.root = Node(6)
-    Bt1.root.right = Node(9)
-    Bt1.root.left = Node(2)
-    Bt1.root.left.left = Node(0)
-    Bt1.root.right.left = Node(10)
+    Bt1.root = Node(8)
+    Bt1.root.right = Node(13)
+    Bt1.root.left = Node(65)
+    Bt1.root.left.left = Node(1)
+    Bt1.root.right.left = Node(70)
     return Bt1
 
 
 @pytest.fixture
 def sec_tree():
     Bt2 = BinaryTree()
-    Bt2.root = Node(6)
-    Bt2.root.right = Node(2)
-    Bt2.root.left = Node(10)
-    Bt2.root.left.left = Node(18) 
+    Bt2.root = Node(8)
+    Bt2.root.right = Node(65)
+    Bt2.root.left = Node(1)
+    Bt2.root.left.left = Node(22) 
     return Bt2
 
 
@@ -26,4 +26,4 @@ def test_intersection(fir_tree, sec_tree):
 
     actual=tree_intersection(fir_tree, sec_tree)
     
-    assert actual == {2, 10, 6}
+    assert actual == {8, 65, 1}
